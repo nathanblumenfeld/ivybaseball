018b9c01414b75260782bd245367dbcd83f72f29
Nathan Blumenfeld <nathanblumenfeld@gmail.com>
2021-11-17 04:34:53 -0500

    +BABIP, WHIP, get_stats fn's, visualizations

--- cornellbaseball/batting_metrics.py
+++ cornellbaseball/batting_metrics.py
@@ -9,7 +9,38 @@ LW_FILEPATH = "data/ncaa_d1_woba_linear_weights.csv"
 # round to 
 ROUND_TO = 3
 
-# BATTING METRICS
+
+def get_season_totals(player_id, season, stats_filepath=STATS_FILEPATH):
+    """
+    A helper function to filter and prepare data for downstream calcuations
+    Returns: pandas.DataFrame object
+    """
+    res = (pd.read_excel(stats_filepath)
+                      .query(f"""player_id=={player_id}""")
+                      .query(f"""season=={season}""")
+                      .fillna(0)
+                     )
+    return res
+
+# CALCULATE BATTING METRICS PER PLAYER PER SEASON 
+def calculate_babip(player_id, season, stats_filepath=STATS_FILEPATH, lw_filepath=LW_FILEPATH, round_to=ROUND_TO):
+    """
+    Returns: BABIP (Batting Average on Balls in Play) for a given player in a given season
+    """
+    player_batting = get_season_totals(player_id, season, stats_filepath)
+    walks = player_batting["BB"].values[0]
+    hits_by_pitch = player_batting["HBP"].values[0]
+    hits = player_batting["H"].values[0]
+    home_runs = player_batting["HR"].values[0]
+    at_bats = player_batting["AB"].values[0]
+    strikeouts = player_batting["K"].values[0]
+    sac_flies = player_batting["SF"].values[0]
+    if (at_bats-strikeouts-home_runs-sac_flies) == 0: 
+        res = 0
+    else: 
+        res = (hits*walks)/(at_bats-strikeouts-home_runs-sac_flies)
+    return round(res, round_to)
+
 def calculate_woba(player_id, season, stats_filepath=STATS_FILEPATH, lw_filepath=LW_FILEPATH, round_to=ROUND_TO):
     """
     Returns: The Weighted On-Base Average for a given player in a given season
@@ -24,11 +55,7 @@ def calculate_woba(player_id, season, stats_filepath=STATS_FILEPATH, lw_filepath
     Parameter year: The season to return for 
     Precondition: year is an int, 2015 <= year <= 2020
     """
-    player_batting = (pd.read_excel(stats_filepath)
-                      .query(f"""player_id=={player_id}""")
-                      .query(f"""season=={season}""")
-                      .fillna(0)
-                     )
+    player_batting = get_season_totals(player_id, season, stats_filepath)
     if len(player_batting) > 0: 
         # get linear weights for given season
         linear_weights = pd.read_csv(lw_filepath)
@@ -57,7 +84,8 @@ def calculate_woba(player_id, season, stats_filepath=STATS_FILEPATH, lw_filepath
         else: 
             res = (wbb*walks+whbp*hits_by_pitch+w1b*singles+w2b*doubles+w3b*triples+whr*home_runs)/plate_appearances
     else: 
-        res = f"""No batting records found for player_id {player_id} within given range"""
+        print(f"""No batting records found for player_id {player_id} within given range""")
+        res = 0
     return round(res, round_to)
 
 def calculate_wraa(player_id, season, stats_filepath=STATS_FILEPATH, lw_filepath=LW_FILEPATH, round_to=ROUND_TO):
@@ -73,11 +101,7 @@ def calculate_wraa(player_id, season, stats_filepath=STATS_FILEPATH, lw_filepath
     Parameter year: The season to return wRC for 
     Precondition: year is an int
     """
-    player_batting = (pd.read_excel(stats_filepath)
-                          .query(f"""player_id=={player_id}""")
-                          .query(f"""season=={season}""")
-                          .fillna(0)
-                      )
+    player_batting = get_season_totals(player_id, season, stats_filepath)
     if len(player_batting) > 0: 
         at_bats = player_batting["AB"].values[0]
         walks = player_batting["BB"].values[0]
@@ -99,7 +123,8 @@ def calculate_wraa(player_id, season, stats_filepath=STATS_FILEPATH, lw_filepath
         else: 
             res = ((((player_woba-league_woba)/woba_scale)))*(plate_appearances)
     else: 
-        res = f"""No batting records found for player_id {player_id} within given range"""
+        print(f"""No batting records found for player_id {player_id} within given range""")
+        res = 0 
     return round(res, round_to)
 
 def calculate_wrc(player_id, season, stats_filepath=STATS_FILEPATH, lw_filepath=LW_FILEPATH, round_to=ROUND_TO):
@@ -118,11 +143,7 @@ def calculate_wrc(player_id, season, stats_filepath=STATS_FILEPATH, lw_filepath=
     Parameter year: The season to return for 
     Precondition: year is an int 
     """
-    player_batting = (pd.read_excel(stats_filepath)
-                          .query(f"""player_id=={player_id}""")
-                          .query(f"""season=={season}""")
-                          .fillna(0)
-                      )
+    player_batting = get_season_totals(player_id, season, stats_filepath)
     if len(player_batting) > 0: 
         at_bats = player_batting["AB"].values[0]
         walks = player_batting["BB"].values[0]
@@ -143,18 +164,15 @@ def calculate_wrc(player_id, season, stats_filepath=STATS_FILEPATH, lw_filepath=
             player_woba = calculate_woba(player_id, season)   
             res = ((((player_woba-league_woba)/woba_scale)+league_runs_per_pa))*plate_appearances
     else:
-        res = f"""No batting records found for player_id {player_id} within given range"""
+        print(f"""No batting records found for player_id {player_id} within given range""")
+        res = 0
     return round(res, ROUND_TO)
 
 def calculate_lw_metrics(player_id, season, stats_filepath=STATS_FILEPATH, lw_filepath=LW_FILEPATH, round_to=ROUND_TO):
     """
     returns: woba, wraa, wrc for given player
     """
-    player_batting = (pd.read_excel(stats_filepath)
-                          .query(f"""player_id=={player_id}""")
-                          .query(f"""season=={season}""")
-                          .fillna(0)
-                         )
+    player_batting = get_season_totals(player_id, season, stats_filepath)
     if len(player_batting) < 0:
         print(f"""no records found for {player_id} in given range""")
         woba = 0 
@@ -189,9 +207,42 @@ def calculate_lw_metrics(player_id, season, stats_filepath=STATS_FILEPATH, lw_fi
             wraa = ((((woba-league_woba)/woba_scale)))*(plate_appearances)
             wrc = ((((woba-league_woba)/woba_scale)+league_runs_per_pa))*plate_appearances
         else: 
+            print(f"""No batting records found for player_id {player_id} within given range""")
             woba = 0 
             wraa = 0
-            wrc = 0
-            
+            wrc = 0            
     return round(woba, round_to), round(wraa, round_to), round(wrc, round_to)
 
+# CALCULATE BATTING METRICS PER PLAYER PER SEASON 
+def get_cornell_batting_stats():
+    """
+    Returns: pandas.DataFrame Object
+    """
+# load data 
+    data = (pd.read_excel("data/cornell/cornell_batting_individual_season_totals_2015_to_2020.xlsx")
+                   .fillna(0)
+           )
+    # set per player per season via uuid of PLAYERID_SEASON _B(atting)
+    data["uuid"] = data.player_id.astype(str)+"_"+data.season.astype(str)+"_b"
+    data = data.set_index("uuid").reset_index()
+    # PA = AB + BB + SF + SH + HBP 
+    data["PA"] =  data["AB"]+data["BB"]+data["SF"]+data["SH"]+data["HBP"]
+    data = data.fillna(0)
+    #  singles = hits-(doubles+triples+home_runs)
+    data["1B"] =  data["H"]-data["2B"]-data["3B"]-data["HR"]
+    # BABIP = (H – HR)/(AB – K – HR + SF)
+    data["BABIP"] = (data["H"]-data["HR"])/(data["AB"]-data["K"]-data["HR"]+data["SF"])
+    # # load in linear weights
+    lw_filepath =  "data/ncaa_d1_woba_linear_weights.csv"
+    linear_weights = (pd.read_csv(lw_filepath)
+                                      .rename(columns={"wOBA":"lgwOBA","Season":"season", "R/PA":"lgR/PA"})
+                                      .loc[:,["season", "lgwOBA", "wOBAScale", "wBB", "wHBP", "w1B", "w2B", "w3B", "wHR", "lgR/PA", "RPG"]]
+                             )
+    df = pd.merge(data, linear_weights, left_on="season", right_on="season", how="left")
+    # wOBA = (wBB×uBB + wHBP×HBP + w1B×1B + w2B×2B + w3B×3B + wHR×HR) / (AB + BB – IBB + SF + HBP)
+    df["wOBA"] =  (((df["wBB"]*df["BB"])+(df["wHBP"]*df["HBP"])+(df["w1B"]*df["1B"])+(df["w2B"]*df["2B"])+(df["w3B"]+df["3B"])+(df["wHR"]*df["HR"])) / df["PA"]).replace(np.inf, 0)
+    # wRAA = [(wOBA−leagueWOBA) / wOBAscale] ∗ PA
+    df["wRAA"] = (((df["wOBA"]-df["lgwOBA"])/df["wOBAScale"])*df["PA"]).replace(np.inf, 0)
+    # wRC = (((wOBA – lgwOBA) / wOBAScale) + (lgR/PA)) * PA
+    df["wRC"] = ((((df["wOBA"]-df["lgwOBA"])/df["wOBAScale"])+df["lgR/PA"])*df["PA"]).replace(np.inf, 0)
+    return df
--- cornellbaseball/pitching_metrics.py
+++ cornellbaseball/pitching_metrics.py
@@ -4,12 +4,30 @@ import numpy as np
 # GLOBAL VARIABLES
 # filepath of Cornell individual season totals
 STATS_FILEPATH = "data/cornell/cornell_pitching_individual_season_totals_2015_to_2020.xlsx"
+
 # filepath of D1 linear weights
 LW_FILEPATH = "data/ncaa_d1_woba_linear_weights.csv"
 # default number of places to round to 
 ROUND_TO = 3
+# FIP Weights
+FIP_HR_WEIGHT = 13
+FIP_BB_WEIGHT = 3
+FIP_K_WEIGHT = 2 
 
-def get_fip(player_id, season, stats_filepath=STATS_FILEPATH, lw_filepath=LW_FILEPATH, round_to=ROUND_TO):
+def get_season_totals(player_id, season, stats_filepath=STATS_FILEPATH):
+    """
+    A helper function to filter and prepare data for downstream calcuations
+    Returns: pandas.DataFrame object
+
+    """
+    res = (pd.read_excel(stats_filepath)
+                      .query(f"""player_id=={player_id}""")
+                      .query(f"""season=={season}""")
+                      .fillna(0)
+                     )
+    return res
+
+def get_fip(player_id, season, stats_filepath=STATS_FILEPATH, lw_filepath=LW_FILEPATH, round_to=ROUND_TO, hr_weight=FIP_HR_WEIGHT, bb_weight=FIP_BB_WEIGHT, k_weight=FIP_K_WEIGHT):
     """
     Returns: Fielding Independent Pitching for a given player in a given season
     
@@ -21,25 +39,20 @@ def get_fip(player_id, season, stats_filepath=STATS_FILEPATH, lw_filepath=LW_FIL
     Parameter year: The season to return wRC for 
     Precondition: year is an int
     """
-    player_pitching = (pd.read_excel(stats_filepath)
-                      .query(f"""player_id=={player_id}""")
-                      .query(f"""season=={season}""")
-                      .fillna(0)
-                     )
-    strike_outs = player_pitching["SO"].values[0]
-    hit_batters = player_pitching["HB"].values[0]
-    walks_given = player_pitching["BB"].values[0]
-    home_runs_allowed = player_pitching["HR-A"].values[0]
-    strike_outs = player_pitching["IP"].values[0]
-    
+    data = get_season_totals(player_id, season, stats_filepath)
+    strike_outs = data["SO"].values[0]
+    hit_batters = data["HB"].values[0]
+    walks_given = data["BB"].values[0]
+    home_runs_allowed = data["HR-A"].values[0]
+    strike_outs = data["IP"].values[0]
     season_weights = (pd.read_csv(lw_filepath)
-                      .query(f"""season=={season}""")
+                      .query(f"""Season=={season}""")
                      )
     fip_constant = season_weights["cFIP"].values[0]
     if strike_outs <= 0: 
         print(f"""warning: player {player_id} has no recorded strikeouts in {season}, cannot calculate FIP""")
         res = 0
-    res = (((13*home_runs_allowed)+(3*(walks_given+hit_batters))-(2*strike_outs))/strike_outs)+fip_constant
+    res = (((hr_weight*home_runs_allowed)+(bb_weight*(walks_given+hit_batters))-(k_weight*strike_outs))/strike_outs)+fip_constant
     return round(res, round_to)
 
 def get_era(player_id, season, stats_filepath=STATS_FILEPATH, lw_filepath=LW_FILEPATH, round_to=ROUND_TO): 
@@ -51,12 +64,8 @@ def get_era(player_id, season, stats_filepath=STATS_FILEPATH, lw_filepath=LW_FIL
     Parameter year: The season to return wRC for 
     Precondition: year is an INT 
     """
-    player_pitching = (pd.read_excel(stats_filepath)
-                      .query(f"""player_id=={player_id}""")
-                      .query(f"""season=={season}""")
-                      .fillna(0)
-                     )
-    res = player_pitching["ERA"].values[0]
+    data = get_season_totals(player_id, season, stats_filepath)
+    res = data["ERA"].values[0]
     return round(res, round_to)
                       
 def get_runs_per_ip(player_id, season, stats_filepath=STATS_FILEPATH, lw_filepath=LW_FILEPATH, round_to=ROUND_TO): 
@@ -64,17 +73,53 @@ def get_runs_per_ip(player_id, season, stats_filepath=STATS_FILEPATH, lw_filepat
     Returns: runs per innings pitched
     
     runs allowed / innings pitched
-
     """
-    player_pitching = (pd.read_excel(stats_filepath)
-                      .query(f"""player_id=={player_id}""")
-                      .query(f"""season=={season}""")
-                      .fillna(0)
-                     )
-    runs_allowed = player_pitching["R"].values[0]
-    innings_pitched = player_pitching["IP"].values[0]
+    data = get_season_totals(player_id, season, stats_filepath)
+    runs_allowed = data["R"].values[0]
+    innings_pitched = data["IP"].values[0]
     if innings_pitched <= 0: 
         res = 0 
     else: 
         res = runs_allowed/innings_pitched
     return round(res, round_to)
+
+def get_whip(player_id, season, stats_filepath=STATS_FILEPATH, lw_filepath=LW_FILEPATH, round_to=ROUND_TO): 
+    """
+    Returns: WHIP: walks and hits per innings pitched
+    WHIP = (BB+H)/IP
+    runs allowed / innings pitched
+    """
+    data = get_season_totals(player_id, season, stats_filepath)
+    walks = data["BB"].values[0]
+    hits = data["H"].values[0]
+    raw_ip = data["IP"].values[0]
+    ip = round(raw_ip,0)+((raw_ip-raw_ip)*3.3)
+    # properly scale IP by multiplying number after decimal point by .33
+    if ip <= 0: 
+        res = 0 
+        print(f"""no records found for {player_id} in {season}""")
+    else:
+        # WHIP = (BB+H)/IP
+        res = round((walks+hits)/ip, round_to)
+    return res
+
+# CALCULATE BATTING METRICS PER PLAYER PER SEASON 
+def get_cornell_pitching_stats(stats_filepath=STATS_FILEPATH, lw_filepath=LW_FILEPATH, round_to=ROUND_TO, hr_weight=FIP_HR_WEIGHT, bb_weight=FIP_BB_WEIGHT, k_weight=FIP_K_WEIGHT):
+    """
+    Returns: DataFrame()
+    """
+    data = (pd.read_excel(stats_filepath)
+                   .fillna(0)
+           )
+    # properly scale IP by multiplying number after decimal point by .33
+    data["IP"] = round(data["IP"],0)+((data["IP"]-round(data["IP"],0))*3.3)
+    linear_weights = (pd.read_csv(lw_filepath)
+                                      .rename(columns={"Season":"season"})
+                                      .loc[:,["cFIP", "season"]]
+                     )
+    df = pd.merge(data, linear_weights, on="season",how="left")
+    #  cFIP = lgERA – (((13 * lgHR) + (3 * (lgBB+lgHBP))-(2 * lgK))/ lgIP)
+    #  FIP = ((13 * HR)+(3 * (BB + HBP))-(2 * K))/IP + cFIP
+    df["FIP"] = round(((((hr_weight*df["HR-A"])+(bb_weight*(df["BB"]+df["HB"]))-(k_weight*df["SO"]))/df["IP"]).replace(np.inf, 0)+df["cFIP"]),round_to)
+    df["WHIP"] = round(((df["BB"]+df["H"])/df["IP"]),round_to) 
+    return df.sort_values(by="FIP", ascending=True)
